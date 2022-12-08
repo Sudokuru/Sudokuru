db3e0f1b4aa3b88a39baef7576c68d5a6d7f5cb5
Galla <gallant.thomas@outlook.com>
2022-12-08 02:43:20 -0500

    Set board strategy to -1 by default and downgraded to typescript 4.9.4!

--- Generator/CustomError.ts
+++ Generator/CustomError.ts
@@ -5,12 +5,12 @@ import { SudokuEnum } from "./Sudoku";
  * @enum
  */
 export enum CustomErrorEnum {
-    INVALID_BOARD_LENGTH = "The board is not length " + SudokuEnum.BOARD_LENGTH,
-    INVALID_BOARD_CHARACTERS = "The board contains characters other than the empty value: " + SudokuEnum.EMPTY_CELL + " and the following candidates: " + SudokuEnum.CANDIDATES,
+    INVALID_BOARD_LENGTH = "The board is not length 9",
+    INVALID_BOARD_CHARACTERS = "The board contains characters other than the empty value: 0 and the following candidates: 123456789",
     BOARD_ALREADY_SOLVED = "The board is already solved",
-    ROW_INDEX_OUT_OF_RANGE = "The row index used isn't in the range 0-" + (SudokuEnum.ROW_LENGTH-1) + ".",
-    COLUMN_INDEX_OUT_OF_RANGE = "The column index used isn't in the range 0-" + (SudokuEnum.COLUMN_LENGTH-1) + ".",
-    INVALID_VALUE = "The value provided isn't one of the following options allowed: " + SudokuEnum.CANDIDATES,
+    ROW_INDEX_OUT_OF_RANGE = "The row index used isn't in the range 0-8",
+    COLUMN_INDEX_OUT_OF_RANGE = "The column index used isn't in the range 0-8",
+    INVALID_VALUE = "The value provided isn't one of the following options allowed: 123456789",
     STRATEGY_NOT_IDENTIFIED = "A strategy hasn't been identified yet",
     UNSOLVABLE = "This board isn't solvable",
     NOT_SOLVED = "This board isn't solved",
