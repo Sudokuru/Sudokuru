e1222ff2f756270fc601b2bba7725df8a37c00f4
Gregory <gregorytravis711@gmail.com>
2022-12-09 22:14:04 -0500

    Changed script to ts and added transpile command to npm start

--- Generator/Demo/script.ts
+++ Generator/Demo/script.ts
@@ -1,66 +1,66 @@
+interface nextStepResponse {
+    board: string[][],
+    notes: string[][],
+    info: string,
+    action: string
+}
+
 async function nextStep() {
-    // Call solver with current board string
-    let url = "http://localhost:3000/solver/nextStep?board=";
-    let boardString = document.getElementById("board").value;
-    //console.log("boardStr" + boardString);
+    // Get input board from user input box and create request url
+    let url:string = "http://localhost:3000/solver/nextStep?board=";
+    let boardInput:HTMLInputElement = <HTMLInputElement>document.getElementById("board");
+    let boardString:string = boardInput.value;
     url += boardString;
 
-    // Await Solvers response
-    let res = await fetch(url);
-    let data = await res.json();
+    // Call and await Solvers response
+    let res:Response = await fetch(url);
+    let data:nextStepResponse = await res.json();
 
     // Set data returned from Solver
-    let board = data.board;
-    console.log("Board Received by Client: " + board);
-    let notes = data.notes;
-    console.log("Notes Received by Client: " + notes);
+    let board:string[][] = data.board;
+    let notes:string[][] = data.notes;
 
     // Store board and notes in sessionStorage
-    let stepNumber;
+    let stepNumber:string;
     if (sessionStorage.getItem("stepNumber") !== null) {
         stepNumber = sessionStorage.getItem("stepNumber");
     }
     else {
         // Convert board string to array
-        let boardArray = new Array();
+        let boardArray:string[][] = new Array();
         for (let i = 0; i < 9; i++) {
             boardArray.push([]);
             for (let j = 0; j < 9; j++) {
                 boardArray[i].push(boardString[(i*9)+j]);
             }
         }
-        console.log(boardArray);
         sessionStorage.setItem("board0", JSON.stringify(boardArray));
         stepNumber = "1";
     }
     sessionStorage.setItem("board" + stepNumber, JSON.stringify(board));
     sessionStorage.setItem("notes" + stepNumber, JSON.stringify(notes));
-    let newStepNumber = (Number(stepNumber) + 1).toString();
+    let newStepNumber:string = (Number(stepNumber) + 1).toString();
     sessionStorage.setItem("stepNumber", newStepNumber);
 
     // Get board and notes from previous step
     let prevStepNumber = (Number(stepNumber) - 1).toString();
     let oldBoard = JSON.parse(sessionStorage.getItem("board" + prevStepNumber));
-    //console.log(oldBoard);
     let oldNotes = JSON.parse(sessionStorage.getItem("notes" + prevStepNumber));
 
-    let noteIndex = 0;
-    let table = document.getElementById("boardTable");
-    let newBoard = "";
-    let value;
-    let values = "123456789";
+    let noteIndex:number = 0;
+    let table:HTMLElement = document.getElementById("boardTable");
+    let newBoard:string = "";
+    let value:string;
+    let values:string = "123456789";
 
-    //console.log("Old Board: " + JSON.stringify(oldBoard));
-    //console.log("New Board:" + JSON.stringify(board));
-
-    for (let row = 0; row < 9; row++) {
-        for (let column = 0; column < 9; column++) {
+    for (let row:number = 0; row < 9; row++) {
+        for (let column:number = 0; column < 9; column++) {
             value = board[row][column];
             newBoard += value;
             if (value === "0" || (value !== oldBoard[row][column])) {
                 value = "";
-                for (let r = 0; r < 3; r++) {
-                    for (let c = 0; c < 3; c++) {
+                for (let r:number = 0; r < 3; r++) {
+                    for (let c:number = 0; c < 3; c++) {
                         if (board[row][column] === oldBoard[row][column]) {
                             if (notes[noteIndex].includes(values[(r*3)+c])) {
                                 value += values[(r*3)+c];
@@ -91,11 +91,11 @@ async function nextStep() {
                 }
             }
             else {
-                table.rows[row].cells[column].style.fontSize = "32px";
+                (<HTMLTableElement>table).rows[row].cells[column].style.fontSize = "32px";
             }
-            table.rows[row].cells[column].innerHTML = value;
+            (<HTMLTableElement>table).rows[row].cells[column].innerHTML = value;
             noteIndex++;
         }
     }
-    document.getElementById("board").value = newBoard;
+    boardInput.value = newBoard;
 }
