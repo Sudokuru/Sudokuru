44b06a0b37554610e4496c99ee71ad63084b5b3e
Gregory <gregorytravis711@gmail.com>
2022-12-10 16:49:28 -0500

    Refactored updateTable and updated README

--- Generator/Demo/script.ts
+++ Generator/Demo/script.ts
@@ -11,24 +11,6 @@ const EMPTY_CELL = "0";
 const SINGLE_NAKED_SINGLE = "439275618051896437876143592342687951185329746697451283928734165563912874714568329";
 const ONLY_NAKED_SINGLES = "310084002200150006570003010423708095760030000009562030050006070007000900000001500";
 
-
-/**
- * Given a board string returns the equivalent board array
- * @param board - board string
- * @returns board array
- */
-function getBoardArray(board: string):string[][] {
-    let boardArray:string[][] = new Array();
-    for (let i = 0; i < 9; i++) {
-        boardArray.push([]);
-        for (let j = 0; j < 9; j++) {
-            boardArray[i].push(board[(i*9)+j]);
-        }
-    }
-    return boardArray;
-}
-
-
 /**
  * Given a board array returns the equivalent board string
  * @param boardArray - board array
@@ -44,66 +26,159 @@ function getBoardString(boardArray: string[][]):string {
     return board;
 }
 
-function updateTable(board:string[][], notes:string[][], info:string, action: string, 
-    stepNumber:number):void {
-    // Change stepNumber if on first step so uses current board for oldBoard
-    // Also disable previous step button if on first step, otherwise enable it
+/**
+ * Disables previous step button if on first step, otherwise enables it
+ * @param stepNumber - current step number
+ */
+function togglePreviousStep(stepNumber:number):void {
     if (stepNumber === 0) {
-        stepNumber = 1;
         (<HTMLButtonElement>document.getElementById("previousStep")).disabled = true;
     }
     else {
         (<HTMLButtonElement>document.getElementById("previousStep")).disabled = false;
     }
-    // Disable nextStep and play buttons if on the last step, otherwise enable them
-    // Also remove hint/info if on last step, otherwise add them
-    if (notes === null) {
-        (<HTMLButtonElement>document.getElementById("nextStep")).disabled = true;
-        (<HTMLButtonElement>document.getElementById("play")).disabled = true;
+    return;
+}
+
+/**
+ * Disables next step button if on last step, enables it otherwise
+ * @param onLastStep - true if on last step
+ */
+function toggleNextStep(onLastStep:boolean):void {
+    (<HTMLButtonElement>document.getElementById("nextStep")).disabled = onLastStep;
+    return;
+}
+
+/**
+ * Disables play button if on last step, enables it otherwise
+ * @param onLastStep - true if on last step
+ */
+function togglePlay(onLastStep:boolean):void {
+    (<HTMLButtonElement>document.getElementById("play")).disabled = onLastStep;
+    return;
+}
+
+/**
+ * Sets hint info and action strings if non-null, otherwise clears them
+ * @param info - hint info
+ * @param action - hint action
+ */
+function setHintText(info:string, action:string):void {
+    if (info === null) {
         (<HTMLParagraphElement>document.getElementById("info")).innerText = "";
         (<HTMLParagraphElement>document.getElementById("action")).innerText = "";
     }
     else {
-        (<HTMLButtonElement>document.getElementById("nextStep")).disabled = false;
-        (<HTMLButtonElement>document.getElementById("play")).disabled = false;
         (<HTMLParagraphElement>document.getElementById("info")).innerText = info;
         (<HTMLParagraphElement>document.getElementById("action")).innerText = action;
     }
+    return;
+}
+
+/**
+ * Checks if is on last step
+ * @param notes - notes array
+ * @returns true if on last step
+ */
+function isOnLastStep(notes:string[][]):boolean {
+    if (notes === null) {
+        return true;
+    }
+    return false;
+}
+
+/**
+ * Updates various UI elements (nextStep/previousStep/play buttons and hint text)
+ * @param stepNumber - step number
+ * @param notes - notes array 
+ * @param info - hint info
+ * @param action - hint action
+ */
+function updateRelatedUI(stepNumber:number, onLastStep:boolean, info:string, action:string): void {
+    // Disables previous step button if on first step, otherwise enables it
+    togglePreviousStep(stepNumber);
+
+    // Disable nextStep and play buttons if on the last step, otherwise enable them
+    toggleNextStep(onLastStep);
+    togglePlay(onLastStep);
+
+    // Remove hint/info if on last step, otherwise add them
+    setHintText(info, action);
+}
+
+/**
+ * Surrounds given value with span to change color to green
+ * @param value - value to be highlighted
+ * @returns green highlighted value
+ */
+function getGreenHighlight(value:string):string {
+    return '<span style="color:green">' + value + '</span>';
+}
+
+/**
+ * Surrounds given value with span to change color to gred
+ * @param value - value to be highlighted
+ * @returns red highlighted value
+ */
+function getRedHighlight(value:string):string {
+    return '<span style="color:red">' + value + '</span>';
+}
+
+/**
+ * Updates table and related UI elements (nextStep/previousStep/play buttons and hint text)
+ * @param board - board array
+ * @param notes - notes array
+ * @param info - hint info
+ * @param action - hint action
+ * @param stepNumber - step number
+ * @returns 
+ */
+function updateTable(board:string[][], notes:string[][], info:string, action: string, 
+    stepNumber:number):void {
+    // Change stepNumber if on first step so uses current board for oldBoard
+    if (stepNumber === 0) {
+        stepNumber = 1;
+    }
+
+    // updates related UI elements (nextStep/previousStep/play buttons and hint text)
+    updateRelatedUI(stepNumber, isOnLastStep(notes), info, action);
+
     // Get board and notes from previous step
     let prevStepNumber = (stepNumber - 1).toString();
     let oldBoard = JSON.parse(sessionStorage.getItem("board" + prevStepNumber));
     let oldNotes = JSON.parse(sessionStorage.getItem("notes" + prevStepNumber));
+
     // Stores value or set of notes that get added to cells in the html Sudoku table
     let value:string;
     // index of note, used to know which to display in each cell of the table
     let noteIndex:number = 0;
     // Sudoku html table
     let table:HTMLElement = document.getElementById("boardTable");
+
+    // updates table
     for (let row:number = 0; row < 9; row++) {
         for (let column:number = 0; column < 9; column++) {
             // Adds notes to the html table cell if cell is empty or had value placed this step
             if (board[row][column] === EMPTY_CELL || (board[row][column] !== oldBoard[row][column])) {
+                // sets font size for notes
                 (<HTMLTableElement>table).rows[row].cells[column].style.fontSize = "16px";
                 value = "";
-                // If value is placed add it to value, otherwise add notes to value and if value placed this step highlights its note green
+                // If value is placed add it to value, otherwise add notes to value
                 for (let r:number = 0; r < 3; r++) {
                     for (let c:number = 0; c < 3; c++) {
-                        // If this value was placed in this cell this step highlight it green, else add normally
+                        // If this value was placed in this cell this step highlight it green
                         // If this value was removed from notes this step highlight it red
+                        // Otherwise add notes normally
                         if (board[row][column] !== oldBoard[row][column] && 
                             CANDIDATES[(r*3)+c] === board[row][column]) {
-                            value += '<span style="color:green">';
-                            value += board[row][column];
-                            value += '</span>';
+                            value += getGreenHighlight(board[row][column]);
                         }
                         else {
                             if (notes[noteIndex].includes(CANDIDATES[(r*3)+c])) {
                                 value += CANDIDATES[(r*3)+c];
                             }
                             else if (oldNotes[noteIndex].includes(CANDIDATES[(r*3)+c])) {
-                                value += '<span style="color:red">';
-                                value += CANDIDATES[(r*3)+c];
-                                value += '</span>';
+                                value += getRedHighlight(CANDIDATES[(r*3)+c]);
                             }
                             else {
                                 value += "-";
@@ -131,12 +206,15 @@ function updateTable(board:string[][], notes:string[][], info:string, action: st
     return;
 }
 
+/**
+ * Decrements nextStep and updates table with previous board state
+ */
 function previousStep() {
     // Return if there is no previous step (stepNumber not intialized)
     if (sessionStorage.getItem("stepNumber") === null) {
         return;
     }
-    // Get stepNumber
+    // newStepNumber is old stepNumber decremented, stepNumber is one below that (0 indexed)
     let stepNumber:string = sessionStorage.getItem("stepNumber");
     let newStepNumber:string = (Number(stepNumber) - 1).toString();
     stepNumber = (Number(newStepNumber) - 1).toString();
