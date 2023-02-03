4a471e60e8f558fd7b51f165e39e54a8fad9cfc4
Gregory <gregorytravis711@gmail.com>
2023-02-03 18:53:10 -0500

    Added writestream code to Generate so it writes JSON array of empty objects for each board string read

--- Generator/Generate.ts
+++ Generator/Generate.ts
@@ -1,18 +1,36 @@
 const events = require('events');
 const fs = require('fs');
 const readline = require('readline');
+const filepath:String = process.argv[2];
 
-let filepath:String = process.argv[2];
+async function main(): Promise<void> {
+    try {
+        const rl = readline.createInterface({
+            input: fs.createReadStream(filepath),
+            crlfDelay: Infinity
+        });
 
-try {
-    const rl = readline.createInterface({
-        input: fs.createReadStream(filepath),
-        crlfDelay: Infinity
-    });
+        let writer = fs.createWriteStream('puzzles.txt', {'flags': 'a'});
+        writer.write("[");
 
-    rl.on('line', (line) => {
-        let board:String = line;
-    });
-} catch (err) {
-    console.log(err);
+        let first:boolean = true;
+        rl.on('line', (line) => {
+            let board:String = line;
+            if (!first) {
+                writer.write(",");
+            }
+            else {
+                first = false;
+            }
+            writer.write("{");
+            writer.write("}");
+        });
+
+        await events.once(rl, 'close');
+        writer.write("]");
+    } catch (err) {
+        console.log(err);
+    }
 }
+
+main();
