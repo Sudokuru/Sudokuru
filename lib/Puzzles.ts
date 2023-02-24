cd2ac9e4f514584d92b5a788436eb3c1459d5df8
Gregory <gregorytravis711@gmail.com>
2023-02-23 23:21:22 -0500

    Updated Puzzles.ts startGame parameters and passed them to server

--- lib/Puzzles.ts
+++ lib/Puzzles.ts
@@ -1,4 +1,5 @@
-const START_GAME:string = "api/v1/user/newGame";
+const START_GAME:string = "api/v1/user/newGame?difficulty=";
+const START_GAME_STRATEGIES:string = "&strategies=";
 
 /**
 * Functions to handle puzzle related operations
@@ -7,13 +8,14 @@ export class Puzzles{
    /**
     * Given a difficulty and an user auth token retrieves a random puzzle close to the difficulty that the user hasn't solved before
     * @param url - server url e.g. http://localhost:3001/
-    * @param difficulty - difficulty integer
+    * @param difficulty - difficulty number (between 0 and 1)
+    * @param strategies - new game can have subset of these strategies
     * @param token - authentication token
     * @returns puzzle JSON object
     */
-   public static async startGame(url: string, difficulty: number, token: string):Promise<JSON> {
+   public static async startGame(url: string, difficulty: number, strategies: string[], token: string):Promise<JSON> {
        try {
-           let res:Response = await fetch(url + START_GAME, {
+           let res:Response = await fetch(url + START_GAME + JSON.stringify(difficulty) + START_GAME_STRATEGIES + JSON.stringify(strategies), {
                method: 'GET',
                headers: {
                    'Content-Type': 'application/json',
