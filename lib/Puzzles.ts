16886632ffd254aac046de4f08836fe01a42540e
Gregory <gregorytravis711@gmail.com>
2023-03-04 18:06:38 -0500

    Added solution as optional parameter to Puzzles.ts getHint

--- lib/Puzzles.ts
+++ lib/Puzzles.ts
@@ -73,9 +73,10 @@ export class Puzzles{
      * @param board - 2d board array (9 arrays, one for each row, each with 9 strings representing values or "0" if empty)
      * @param notes - 2d notes array (81 arrays, one for each cell containing each note that is left in it)
      * @param strategies - optional parameter specifying which strategies to use
+     * @param solution - optional parameter specifying solution to the given board, used in amend notes strategy to correct user mistakes
      * @returns JSON object containing hint data
      */
-    public static getHint(board: string[][], notes: string[][], strategies?: string[]):JSON {
+    public static getHint(board: string[][], notes: string[][], strategies?: string[], solution?: string[][]):JSON {
         let algorithm:number[];
         if (strategies !== undefined) {
             algorithm = new Array();
@@ -88,7 +89,7 @@ export class Puzzles{
                 }
             }
         }
-        let solver:Solver = new Solver(board, algorithm, notes);
+        let solver:Solver = new Solver(board, algorithm, notes, solution);
         let hint:Hint = solver.nextStep();
         return <JSON><unknown>{
             "strategy": hint.getStrategy(),
