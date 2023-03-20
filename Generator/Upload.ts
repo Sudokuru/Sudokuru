f4749feae2007311fa239aa2305ef76dbee3f0e1
Gregory <gregorytravis711@gmail.com>
2023-03-19 23:32:56 -0400

    Added throttling to CLI, updated readme, and incremented package versions

--- Generator/Upload.ts
+++ Generator/Upload.ts
@@ -2,6 +2,7 @@ const fs = require('fs');
 const readline = require('readline');
 const endpoint = process.argv[2];
 const token = process.argv[3];
+const throttle = Number(process.argv[4]);
 
 async function main(): Promise<void> {
     try {
@@ -10,15 +11,28 @@ async function main(): Promise<void> {
             crlfDelay: Infinity
         });
 
+        let batches:string[] = new Array();
         let inComment:boolean = false;
+
+        // Read batches of puzzles to be uploaded
         rl.on('line', async (line) => {
             if (line[0] === '/' && line[1] === '*') {
                 inComment = true;
             }
             else if (!inComment) {
+                batches.push(line);
+            }
+            else if (line[0] === '*' && line[1] === '/') {
+                inComment = false;
+            }
+        });
+
+        // Upload puzzle batches with given delay in between each upload
+        rl.on('close', async () => {
+            for (let i:number = 0; i < batches.length; i++) {
                 let res:Response = await fetch(endpoint, {
                     method: 'POST',
-                    body: line,
+                    body: batches[i],
                     headers: {
                         'Content-Type': 'application/json',
                         'Authorization': 'Bearer ' + token
@@ -26,9 +40,9 @@ async function main(): Promise<void> {
                 });
                 let data = await res.json();
                 console.log(data);
-            }
-            else if (line[0] === '*' && line[1] === '/') {
-                inComment = false;
+                if (i < (batches.length - 1)) {                
+                    await new Promise(f => setTimeout(f, throttle));
+                }
             }
         });
     } catch(err) {
