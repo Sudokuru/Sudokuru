3a11cb10529dc97bd62c80ca6da67b246f548c95
Galla <gallant.thomas@outlook.com>
2023-03-22 05:35:16 -0400

    Exporting lessons

--- Generator/index.ts
+++ Generator/index.ts
@@ -2,3 +2,4 @@ export {Board} from "./Board";
 export {StrategyEnum} from "./Sudoku";
 export {Puzzles} from "../lib/Puzzles";
 export {Drills} from "../lib/Drills";
+export {Lessons} from "../lib/Lessons";
