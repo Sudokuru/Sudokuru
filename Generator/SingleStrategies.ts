2998e3a25bfc1a9bd4e29dd4a8ec97419ac8d8bf
Gregory Travis <gregorytravis711@gmail.com>
2023-07-22 12:49:14 -0400

    Added hidden singles to SingleStrategies getSingle and corresponding unit tests

--- Generator/SingleStrategies.ts
+++ Generator/SingleStrategies.ts
@@ -1,4 +1,5 @@
 import { Cell } from "./Cell";
+import { Group } from "./Group";
 import { SudokuEnum } from "./Sudoku";
 
 /**
@@ -16,6 +17,44 @@ export class SingleStrategies{
         // Check if the cell is a naked single
         if (board[row][column].getNotes().getSize() === 1){
             return board[row][column].getNotes().lower_bound(0);
+        }
+        // Check if the cell is a hidden single
+        let hiddenSingle:Group = board[row][column].getNotes().clone(); // hidden single candidates
+        // Check row
+        for (let c:number = 0; c < SudokuEnum.ROW_LENGTH; c++){
+            if (c !== column){
+                // Remove candidates from hiddenSingle that are in the cell at (row, c)
+                hiddenSingle.remove(board[row][c].getNotes());
+            }
+        }
+        if (hiddenSingle.getSize() === 1){
+            return hiddenSingle.lower_bound(0);
+        }
+        // Check column
+        hiddenSingle = board[row][column].getNotes().clone();
+        for (let r:number = 0; r < SudokuEnum.COLUMN_LENGTH; r++){
+            if (r !== row){
+                // Remove candidates from hiddenSingle that are in the cell at (r, column)
+                hiddenSingle.remove(board[r][column].getNotes());
+            }
+        }
+        if (hiddenSingle.getSize() === 1){
+            return hiddenSingle.lower_bound(0);
+        }
+        // Check box
+        hiddenSingle = board[row][column].getNotes().clone();
+        let boxRowStart:number = board[row][column].getBoxRowStart();
+        let boxColumnStart:number = board[row][column].getBoxColumnStart();
+        for (let r:number = boxRowStart; r < boxRowStart + SudokuEnum.BOX_LENGTH; r++){
+            for (let c:number = boxColumnStart; c < boxColumnStart + SudokuEnum.BOX_LENGTH; c++){
+                if (r !== row || c !== column){
+                    // Remove candidates from hiddenSingle that are in the cell at (r, c)
+                    hiddenSingle.remove(board[r][c].getNotes());
+                }
+            }
+        }
+        if (hiddenSingle.getSize() === 1){
+            return hiddenSingle.lower_bound(0);
         }
         return -1;
     }
--- Generator/tests/unit/SingleStrategies.test.ts
+++ Generator/tests/unit/SingleStrategies.test.ts
@@ -21,3 +21,51 @@ describe("check cell for naked single", () => {
         expect(SingleStrategies.getSingle(board, 0, 0)).toBe(8);
     });
 });
+
+describe("check cell for hidden single", () => {
+    it('should find a row hidden single', () => {
+        let board:Cell[][] = getBlankCellBoard();
+        // Remove 8 from all cells in row 0 except for (0, 0)
+        let cellBoard:CellBoard = new CellBoard(board);
+        cellBoard.resetNotes(0, 0);
+        let notes:Group = new Group(false);
+        notes.insert(8);
+        for (let column:number = 1; column < SudokuEnum.ROW_LENGTH; column++){
+            cellBoard.resetNotes(0, column);
+            cellBoard.removeNotes(0, column, notes);
+        }
+        expect(SingleStrategies.getSingle(board, 0, 0)).toBe(8);
+    });
+    it('should find a column hidden single', () => {
+        let board:Cell[][] = getBlankCellBoard();
+        // Remove 8 from all cells in column 0 except for (0, 0)
+        let cellBoard:CellBoard = new CellBoard(board);
+        cellBoard.resetNotes(0, 0);
+        let notes:Group = new Group(false);
+        notes.insert(8);
+        for (let row:number = 1; row < SudokuEnum.COLUMN_LENGTH; row++){
+            cellBoard.resetNotes(row, 0);
+            cellBoard.removeNotes(row, 0, notes);
+        }
+        expect(SingleStrategies.getSingle(board, 0, 0)).toBe(8);
+    });
+    it('should find a box hidden single', () => {
+        let board:Cell[][] = getBlankCellBoard();
+        // Remove 8 from all cells in box 0 except for (0, 0)
+        let cellBoard:CellBoard = new CellBoard(board);
+        cellBoard.resetNotes(0, 0);
+        let notes:Group = new Group(false);
+        notes.insert(8);
+        let boxRowStart:number = 0;
+        let boxColumnStart:number = 0;
+        for (let row:number = boxRowStart; row < boxRowStart + SudokuEnum.BOX_LENGTH; row++){
+            for (let column:number = boxColumnStart; column < boxColumnStart + SudokuEnum.BOX_LENGTH; column++){
+                if (row !== 0 || column !== 0){
+                    cellBoard.resetNotes(row, column);
+                    cellBoard.removeNotes(row, column, notes);
+                }
+            }
+        }
+        expect(SingleStrategies.getSingle(board, 0, 0)).toBe(8);
+    });
+});
