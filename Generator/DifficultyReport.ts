63de368d2f3f1c7f2c2485c7a710e7db3cea589e
Gregory Travis <gregorytravis711@gmail.com>
2023-07-29 13:45:11 -0400

    Switched DifficultyReport to use rd score calculated by Board class

--- Generator/DifficultyReport.ts
+++ Generator/DifficultyReport.ts
@@ -5,10 +5,7 @@
  */
 
 import { Board } from "./Board";
-import { Cell } from "./Cell";
-import { Dependency } from "./Dependency";
-import { Refutation } from "./Refutation";
-import { StrategyEnum, getBoardArray, getCellBoard, simplifyNotes } from "./Sudoku";
+import { StrategyEnum } from "./Sudoku";
 
 let puzzles:string[] = ["100603700700240560304795080800002400046300100000500008003000809000050307020830010", "000005008000804109002090540230080654700650800085031002020000000050208716008100000", "007900100000700060630000407006000200800000706000008005902004800450000900000000001", "090000060000100450306000100005002001800000040027450000000907000002000000001020800", "006031070437005000010467008029178300000000026300050000805004910003509087790086004", "130000904940830200008500003009005370400900061080060020004107002802306745357200000", "000600080609830000007009610000080190200300000006057000000405008005760400370900501", "050000072009050030080600549006002150100740000500000004090000010734500920800400000",
                         "000006201800000000100200705300010006420700050000000040000002000057030090603509000", "009600001010094027000800040008170400007300000090240053070000902105002000000700000", "000060008000100700604000200030000000090070010800000093028007000005008000000319000", "005307000902500000070020016030000040600000200000905000090000003010000704000482000", "830000009000050000006407200005020000000010300600305080000100000040000020007603400", "060009300070010000301002040010000000000004008905020700000900070203050900600000000", "605107809079403020000080037060500000201000903000002010790030000010904360304801705", "431560000000040000700020040017600900003409100009005680080030002000090000000051863",
@@ -38,29 +35,9 @@ function getStrategyString(strategies:boolean[]):string {
 let puzzlesArray = [];
 
 for (let i:number = 0; i < puzzles.length; i++) {
-    let board:Cell[][] = getCellBoard(getBoardArray(puzzles[i]));
-    // Add all notes to board
-    for (let r:number = 0; r < 9; r++) {
-        for (let c:number = 0; c < 9; c++) {
-            if (board[r][c].isEmpty()) {
-                board[r][c].resetNotes();
-            }
-        }
-    }
-    // Simplify notes based on givens
-    for (let r:number = 0; r < 9; r++) {
-        for (let c:number = 0; c < 9; c++) {
-            if (!board[r][c].isEmpty()) {
-                simplifyNotes(board, r, c);
-            }
-        }
-    }
-    let boardObj:Board = new Board(puzzles[i]);
-    let solution:string[][] = boardObj.getSolution();
-    let refutationScore:number = Refutation.getRefutationScore(board, solution, 1);
-    let dependencyScore:number = Dependency.getDependencyScore(board);
-    let rdScore:number = refutationScore + (-1 * dependencyScore);
-    puzzlesArray.push(new Puzzle(puzzles[i], sources[i], descriptions[i], getStrategyString(boardObj.getStrategies()), 81 - (puzzles[i].match(/0/g) || []).length, rdScore));
+    let board:Board = new Board(puzzles[i]);
+    let rdScore:number = board.getDifficulty();
+    puzzlesArray.push(new Puzzle(puzzles[i], sources[i], descriptions[i], getStrategyString(board.getStrategies()), 81 - (puzzles[i].match(/0/g) || []).length, rdScore));
 }
 
 puzzlesArray.sort((a, b) => (a.rdScore > b.rdScore) ? 1 : -1);
