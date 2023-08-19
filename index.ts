23460697fccb4c9416218b3b9a342dc4377b9f50
Galla <gallant.thomas@outlook.com>
2023-08-19 09:09:01 -0400

    Removing legacy difficulty conversion

--- index.ts
+++ index.ts
@@ -2,12 +2,11 @@
 import {Board} from "./Generator/Board";
 import {StrategyEnum} from "./Generator/Sudoku";
 // Functions
-import { difficultyConversion } from "./lib/Difficulty";
 import { getHint } from "./lib/Hint";
 import { calculateNotes } from "./lib/Drill";
 // Types
 import {sudokuStrategy, sudokuStrategyArray } from "./lib/Api";
 
 
-export { Board, StrategyEnum, difficultyConversion, getHint, calculateNotes };
+export { Board, StrategyEnum, getHint, calculateNotes };
 export { sudokuStrategy, sudokuStrategyArray };
