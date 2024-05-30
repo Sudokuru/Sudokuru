d9f37f68bf63317cc8eb87d24111330e4d1b46b9
Gregory Travis <gregorytravis711@gmail.com>
2024-05-30 19:25:25 -0400

    Let there be Const!

--- Generator/tests/functional/Hint.test.ts
+++ Generator/tests/functional/Hint.test.ts
@@ -5,8 +5,8 @@ import { Solver } from "../../Solver";
 
 describe("get hints", () => {
     it('get hint works with or without some optional parameters', () => {
-        let board:string[][] = getBoardArray(TestBoards.SINGLE_NAKED_SINGLE);
-        let solver:Solver = new Solver(board);
+        const board:string[][] = getBoardArray(TestBoards.SINGLE_NAKED_SINGLE);
+        const solver:Solver = new Solver(board);
         let notes:string[][] = solver.getNotes();
         let hint:JSON = getHint(board, notes, undefined, getBoardArray(TestBoards.SINGLE_NAKED_SINGLE_SOLUTION));
         expect(hint["strategy"]).toBe("AMEND_NOTES");
@@ -17,11 +17,11 @@ describe("get hints", () => {
     });
     
     it('get hint uses provided strategies in given order precedence', () => {
-        let board:string[][] = getBoardArray(TestBoards.ONLY_NAKED_SINGLES);
-        let solver:Solver = new Solver(board);
-        let notes:string[][] = solver.getNotes();
+        const board:string[][] = getBoardArray(TestBoards.ONLY_NAKED_SINGLES);
+        const solver:Solver = new Solver(board);
+        const notes:string[][] = solver.getNotes();
         notes[2] = ["6"];
-        let strategies:string[] = ["NAKED_SINGLE", "AMEND_NOTES"];
+        const strategies:string[] = ["NAKED_SINGLE", "AMEND_NOTES"];
         let hint:JSON = getHint(board, notes, strategies);
         expect(hint["strategy"]).toBe("NAKED_SINGLE");
         // Versus the following if using default strategy order precedence which has amend notes > naked single
