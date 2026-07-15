e0248c6c24dbd2e35b3a9335159f4f4d7ce779a9
Gregory Travis <gregorytravis711@gmail.com>
2026-07-14 21:16:09 -0400

    added get wrong value hint stub

--- V4/wrongValue.ts
+++ V4/wrongValue.ts
@@ -1,5 +1,8 @@
 import type { CellLocation, CellProps, Hint, SudokuValue } from "./Types";
 
+/**
+ * Returns a staged removal hint when the targeted user value differs from the solution.
+ */
 export declare function getWrongValueHint(
   puzzle: CellProps[][],
   solution: SudokuValue[][],
